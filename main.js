a8aa6e628f6a919ae916fad93e0f157f176a8374
Marianna <mariannamelendez0504@gmail.com>
2021-10-16 19:18:57 -0400

    subiendo ig

---
+++ main.js
@@ -0,0 +1,11 @@
+
+const stories = document.querySelectorAll('#stories-container button');
+
+stories.forEach(story => {
+    story.addEventListener('click', e => {
+        stories.forEach(s => {s.classList.remove('active')});
+        if(story.querySelector('.profile').classList.contains('vacio')) return false;
+        
+        story.classList.add('active');
+    });
+});
